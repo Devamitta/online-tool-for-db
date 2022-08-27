911a93f4260e9a790e45bbe98494de47f5a8ecb3
Falko Strenzke <{ID}+{username}@users.noreply.github.com>
2022-08-27 12:46:11 +0200

    added pdf

--- design/001_online_editing_basic/build_html.sh
+++ design/001_online_editing_basic/build_html.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 pandoc 001_online_editing_basic.md --filter pandoc-plantuml -o 001_online_editing_basic.html
+pandoc 001_online_editing_basic.md --filter pandoc-plantuml -o 001_online_editing_basic.pdf
