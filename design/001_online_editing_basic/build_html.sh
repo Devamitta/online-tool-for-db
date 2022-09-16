e534c98dbfc8d99937aae3f31aac46c9b1259fac
Falko Strenzke <{ID}+{username}@users.noreply.github.com>
2022-09-16 09:12:18 +0200

    added description of ODS data and data model for roots

--- design/001_online_editing_basic/build_html.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#pandoc 001_online_editing_basic.md --filter pandoc-plantuml -o 001_online_editing_basic.html
-pandoc 001_online_editing_basic.md --filter pandoc-plantuml -o 001_online_editing_basic.pdf
